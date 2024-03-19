ec46af5b5b0b5af43d29ce6c3217e04840b0a7df
happyo <xiaxiongzhi@corp.netease.com>
2024-03-19 18:20:08 +0800

    @dev fix swift build error

--- Package.swift
+++ Package.swift
@@ -3,13 +3,11 @@
 
 import PackageDescription
 
-let enablePreviews = false
-
 let package = Package(
     name: "Pow",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
+        .macOS(.v13),
         .macCatalyst(.v15)
     ],
     products: [
@@ -21,15 +19,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/EmergeTools/SnapshotPreviews-iOS", exact: "0.7.6")
+      // .package(url: "https://github.com/EmergeTools/SnapshotPreviews-iOS", exact: "0.7.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Pow",
-            dependencies: enablePreviews ? [.product(name: "SnapshotPreferences", package: "SnapshotPreviews-iOS", condition: .when(platforms: [.iOS]))] : [],
-            swiftSettings: enablePreviews ? [.define("EMG_PREVIEWS")] : nil),
+            dependencies: [],
+            resources: [
+                .process("Resources") // 指定资源目录
+            ]),
         .testTarget(
             name: "PowTests",
             dependencies: ["Pow"]),
